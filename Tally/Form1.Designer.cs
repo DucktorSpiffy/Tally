2439956498302cd58fec7bc337aeace84fad5064
DucktorSpiffy <30412733+DucktorSpiffy@users.noreply.github.com>
2018-04-15 21:55:21 +0100

    Added code

---
+++ Tally/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Tally
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NumberDisplay = new System.Windows.Forms.TextBox();
+            this.Add = new System.Windows.Forms.Button();
+            this.TakeAway = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.Reset = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // NumberDisplay
+            // 
+            this.NumberDisplay.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Bold);
+            this.NumberDisplay.Location = new System.Drawing.Point(12, 28);
+            this.NumberDisplay.Name = "NumberDisplay";
+            this.NumberDisplay.ReadOnly = true;
+            this.NumberDisplay.Size = new System.Drawing.Size(260, 40);
+            this.NumberDisplay.TabIndex = 0;
+            this.NumberDisplay.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Add
+            // 
+            this.Add.Font = new System.Drawing.Font("Verdana", 30F, System.Drawing.FontStyle.Bold);
+            this.Add.Location = new System.Drawing.Point(105, 78);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(75, 75);
+            this.Add.TabIndex = 1;
+            this.Add.Text = "+";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // TakeAway
+            // 
+            this.TakeAway.Font = new System.Drawing.Font("Verdana", 30F, System.Drawing.FontStyle.Bold);
+            this.TakeAway.Location = new System.Drawing.Point(105, 159);
+            this.TakeAway.Name = "TakeAway";
+            this.TakeAway.Size = new System.Drawing.Size(75, 75);
+            this.TakeAway.TabIndex = 1;
+            this.TakeAway.Text = "-";
+            this.TakeAway.UseVisualStyleBackColor = true;
+            this.TakeAway.Click += new System.EventHandler(this.TakeAway_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(114, 22);
+            this.toolStripMenuItem2.Text = "Save As";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click_1);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // Reset
+            // 
+            this.Reset.Font = new System.Drawing.Font("Verdana", 12F);
+            this.Reset.Location = new System.Drawing.Point(105, 240);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(75, 26);
+            this.Reset.TabIndex = 3;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click_1);
+            // 
+            // Tally
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 292);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.TakeAway);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.NumberDisplay);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Tally";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Display;
+        private System.Windows.Forms.Button Plus;
+        private System.Windows.Forms.Button Minus;
+        private System.Windows.Forms.MenuStrip Menu;
+        private System.Windows.Forms.ToolStripMenuItem Open;
+        private System.Windows.Forms.ToolStripMenuItem Savwe;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.TextBox NumberDisplay;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button TakeAway;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Button Reset;
+    }
+}
+
---
+++ Tally/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Tally : Form
+    {
+        private int CurrentNumber=0;
+        public event System.EventHandler NumChanged;
+        public int Num {
+            get {
+                return CurrentNumber;
+            }
+
+            set {
+                //#3
+                CurrentNumber = value;
+                UpdateNumber();
+            }
+        }
+        private string CurOpenFile; //so we can Save after Saving as
+        public Tally()
+        {
+            InitializeComponent();
+            Num = 0;
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            Num++;
+        }
+
+        private void TakeAway_Click(object sender, EventArgs e)
+        {
+            if (Num > 0)
+            {
+                Num--;
+            }
+        }
+
+        private void UpdateNumber()
+        {
+            //Update the display
+            NumberDisplay.Text = Num.ToString();
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            //Reset number back to 0
+            Num=0;
+        }
+
+        //#2
+        protected virtual void OnNumberChanged()
+        {
+            //create an event for when 'Num' is changed (so we don't have to explicitly call the function in Minus_Click, button1_click and Reset_Click
+            if (NumChanged != null) NumChanged(this, EventArgs.Empty);
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+
+            save.FileName = "NewTally.txt";
+
+            save.Filter = "Text File | *.txt";
+            
+            if (save.ShowDialog() == DialogResult.OK)
+
+            {
+
+                StreamWriter writer = new StreamWriter(save.OpenFile());
+
+                writer.WriteLine(Num.ToString());
+                writer.Dispose();
+
+                writer.Close();
+                CurOpenFile = Path.GetFullPath(save.FileName);
+                Console.WriteLine(CurOpenFile);
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(CurOpenFile))
+            {
+                string newTally = Num.ToString();
+
+                File.WriteAllText(CurOpenFile, newTally);
+            }    
+        }
+
+        private void Reset_Click_1(object sender, EventArgs e)
+        {
+            Num = 0;
+        }
+
+        private void openToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Text File | *.txt";
+            openFileDialog1.Title = "Select a Tally File";
+
+            // Show the Dialog.  
+            // If the user clicked OK in the dialog and  
+            // a .CUR file was selected, open it.  
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                // Assign the cursor in the Stream to the Form's Cursor property.  
+                string newTally = File.ReadAllText(openFileDialog1.FileName);
+                Num = int.Parse(newTally);
+                CurOpenFile = Path.GetFullPath(openFileDialog1.FileName);
+            }
+        }
+    }
+}
+
+
